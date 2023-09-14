fc00bc19b8913c59b02fed88b973ffce7d2bdb63
Florian Betin <florian.betin.pro@gmail.com>
2023-09-14 19:11:19 +0200

    Q1 v.wcs

--- app.js
+++ app.js
@@ -4,56 +4,74 @@ const app = express();
 
 const port = 5000;
 
-  const movies = [
-    {
-      id: 1,
-      title: "Citizen Kane",
-      director: "Orson Wells",
-      year: "1941",
-      colors: false,
-      duration: 120,
-    },
-    {
-      id: 2,
-      title: "The Godfather",
-      director: "Francis Ford Coppola",
-      year: "1972",
-      colors: true,
-      duration: 180,
-    },
-    {
-      id: 3,
-      title: "Pulp Fiction",
-      director: "Quentin Tarantino",
-      year: "1994",
-      color: true,
-      duration: 180,
-    },
-  ];
-  
-  app.listen(port, (err) => {
-    if (err) {
-      console.error("Something bad happened");
-    } else {
-      console.log(`Server is listening on ${port}`);
-    }
-  });
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
 
-  //Route en get, path '/'  et fonction reçu (req, res) => {res.send("Welcome to Express");}
-app.get("/", (req, res) => {
-  res.send("Welcome to my favorite movie list");
+app.get("/", welcome);
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+const getMovies = (req, res) => {
+  res.json(movies);
+};
+
+app.get("/api/movies", getMovies);
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const movie = movies.find((movie) => movie.id === id);
+
+  if (movie != null) {
+    res.json(movie);
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+
+app.get("/api/movies/:id", getMovieById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
 });
 
-  const getMovies = (req, res) => {
-    res.status(200).json(movies);
-  };
+//   app.get('/api/products/:productID', (req, res) => {
+//     const id = Number(req.params.productID)
+//     const product = products.find(product => product.id === id)
 
-  app.get("/api/movies", getMovies)
-
-  //
-
-  const getMoviesById = (req, res) => {
-    res.json(movies).find((element) => element = `${req.params.id}`)
-  };
-
-  app.get("/api/movies/:id", getMoviesById)
+//         if (!product) {
+//         return res.status(404).send('Product not found')
+//     }
+//     res.json(product)
+// }
+// )
